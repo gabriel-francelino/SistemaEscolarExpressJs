57eaf3c89f40154f65f90c02e9d90916949e827d
Gabriel Francelino <gabriel.francelino@sou.unifal-mg.edu.br>
2023-10-17 20:45:46 -0300

    instalando morgan para registrar as requisições

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # CursoExtensaoNodeJs
+
+necessário instalar as seguintes bibliotecas, caso não tenha:
+
+npm install express
+npm install nodemon
+npm install morgan
+
+Para executar:
+
+npm run start ou npm run dev
+
--- src/rotas/professor.js
+++ src/rotas/professor.js
@@ -1,25 +1,14 @@
 const express = require('express');
+const controlador = require('../controladores/professor');
 
 const router = express.Router();
 
-router.get('/professor', (req, res) => {
-    res.send('[GET] Professor');
-    }
-);
+router.get('/professor', controlador.get);
 
-router.post('/professor', (req, res) => {
-    res.send('[GET] Professor');
-    }
-);
+router.post('/professor', controlador.post);
 
-router.delete('/professor', (req, res) => {
-    res.send('[DELETE] Professor');
-    }
-);
+router.delete('/professor', controlador.delete);
 
-router.patch('/professor', (req, res) => {
-    res.send('[PATCH] Professor');
-    }
-);
+router.patch('/professor', controlador.patch);
 
 module.exports = router;
